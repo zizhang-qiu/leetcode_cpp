6700db18da49cefb9db010c941f01c8611e43436
hanyuu1 <13738325103@126.com>
2024-02-08 09:39:03 +0800

    update 993.cc

--- src/01bag.cc
+++ src/01bag.cc
@@ -6,22 +6,21 @@
 using namespace std;
 
 void test_2_wei_bag_problems() {
-  vector<int> weight = {1, 3, 4}; //物体质量
-  vector<int> value = {15, 20, 30}; //物体价值
-  int bagweight = 4; //背包的最大容量
+  vector<int> weight = {1, 3, 4};
+  vector<int> value = {15, 20, 30};
+  int bagweight = 4; // Max capacity of bag
 
-  //数组定义
+  // dp vector.
   vector<vector<int>> dp(weight.size(), vector<int>(bagweight + 1, 0));
 
-  //初始化
+  // initialize.
   for (int j = weight[0]; j <= bagweight; j++) {
     dp[0][j] = value[0];
   }
 
-  //weight数组的大小，物品的个数
-  for (int i = 1; i < weight.size(); i++) //遍历物品
+  for (int i = 1; i < weight.size(); i++) // iterate items
   {
-    for (int j = 0; j <= bagweight; j++) //遍历背包容量
+    for (int j = 0; j <= bagweight; j++) // iterate bag capacity
     {
       if (j < weight[i]) dp[i][j] = dp[i - 1][j];
       else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
