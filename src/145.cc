f0159b1f00528c809264f7e08a48255e16b2073d
hanyuu1 <13738325103@126.com>
2024-02-12 09:47:02 +0800

    update 145.cc

---
+++ src/145.cc
@@ -0,0 +1,35 @@
+//
+// Created by qzz on 2024/2/12.
+//
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  void dfs(TreeNode* node, vector<int>& ans){
+    if (node->left){
+      dfs(node->left, ans);
+    }
+    if (node->right){
+      dfs(node->right, ans);
+    }
+    ans.push_back(node->val);
+  }
+  vector<int> postorderTraversal(TreeNode* root) {
+    if (root == nullptr) {
+      return {};
+    }
+    vector<int> ans{};
+    dfs(root, ans);
+    return ans;
+  }
+};
