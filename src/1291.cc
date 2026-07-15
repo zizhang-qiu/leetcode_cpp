2457400902be5ec3e06bf8aa4c6e2911671e0582
hanyuu1 <13738325103@126.com>
2026-07-15 09:10:13 +0800

    Update

---
+++ src/1291.cc
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> sequentialDigits(int low, int high) {
+    constexpr int kNumNumbers = 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1;
+    constexpr std::array<int, kNumNumbers> arr = {
+        1,       2,       3,        4,        5,        6,      7,      8,
+        9,       12,      23,       34,       45,       56,     67,     78,
+        89,      123,     234,      345,      456,      567,    678,    789,
+        1234,    2345,    3456,     4567,     5678,     6789,   12345,  23456,
+        34567,   45678,   56789,    123456,   234567,   345678, 456789, 1234567,
+        2345678, 3456789, 12345678, 23456789, 123456789};
+
+    auto b = std::lower_bound(std::begin(arr), std::end(arr), low);
+    auto e = std::upper_bound(std::begin(arr), std::end(arr), high);
+    return std::vector<int>(b, e);
+  }
+};
+
+int main() {
+    Solution sl;
+    auto ans = sl.sequentialDigits(100, 300);
+    for(int a: ans){
+        std::cout << a << " ";
+    }
+    std::cout << "\n";
+}
---
+++ src/3658.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+int main(){
+
+}
