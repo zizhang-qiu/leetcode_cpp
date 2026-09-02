d14a2b66409a2d6d5ce4cc8db106cc8648d46c0e
hanyuu1 <13738325103@126.com>
2026-09-02 09:13:57 +0800

    Update 3875

---
+++ src/3875.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
+
+int main() {
+    
+}
